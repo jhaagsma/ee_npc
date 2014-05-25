8ad2a5cb2f1475a33eebe6b60dc5b34b686222f0
qzjul <jhaagsma@gmail.com>
2014-05-25 15:40:50 -0600

    documentation

--- config_example.php
+++ config_example.php
@@ -1,7 +1,12 @@
 <?php
+/*
+You must copy or save this file to config.php
+and fill out your AI API KEY and your USERNAME
+*/
+
 
 $config = array(
-	'ai_key' => 'your-ai-key-here',
-	'base_url' => 'http://www.earthempires.com/api',
+	'ai_key' => 'your-ai-api-key-here',
 	'username' => 'your-username-here'
+	'base_url' => 'http://www.earthempires.com/api',	//Don't change this unless qz tells you to =D it needs to end in /api either way
 );
