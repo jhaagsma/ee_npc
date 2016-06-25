04d71af5a2de3387d87b059bcdc5dfecae35a760
qzjul <jhaagsma@gmail.com>
2016-06-24 20:11:13 -0600

    tweaking goals fn

--- farmer_strat.php
+++ farmer_strat.php
@@ -202,7 +202,7 @@ function buy_farmer_goals(&$c, $spend = null, $spend_partial = null, $skip = 0)
         $skip++;
     }
 
-    $spend -= $diff;
+    $spend -= $spend_partial;
     if ($spend > 10000 && $skip < count($score) - 1) {
         buy_farmer_goals($c, $spend, $spend_partial, $skip);
     }
