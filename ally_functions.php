f14d351441ec6447668b4be292a4fbf3752b1e5e
qzjul <jhaagsma@gmail.com>
2018-03-10 16:04:08 -0700

    add ally functions

---
+++ ally_functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file has all the ally functions
+ *
+ * PHP Version 7
+ *
+ * @category Functions
+ * @package  EENPC
+ * @author   Julian Haagsma aka qzjul <jhaagsma@gmail.com>
+ * @license  MIT License
+ * @link     https://github.com/jhaagsma/ee_npc/
+ */
+
+namespace EENPC;
