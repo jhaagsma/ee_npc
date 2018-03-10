bf6fc50c227252ca8f8b0048937af7da992ac21c
qzjul <jhaagsma@gmail.com>
2018-03-10 16:37:09 -0700

    add getCandidates

--- ally_functions.php
+++ ally_functions.php
@@ -24,7 +24,23 @@ class Allies
     public static function getList()
     {
         $result = ee('ally/list');
-        out("RESULT: $result->govt");
+        out("Ally List");
+        out($result);
         return $result;
     }//end getList()
+
+    /**
+     * Get the list of candidates
+     *
+     * @param string $type String of ally type
+     *
+     * @return Result The result
+     */
+    public static function getCandidates($type = 'def')
+    {
+        $result = ee('ally/candidates', ['type' => $type]);
+        out("Ally Candidates");
+        out($result);
+        return $result;
+    }//end getCandidates()
 }//end class
