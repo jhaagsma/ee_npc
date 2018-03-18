d7ee3cf1e0c19e06f1d9f557b62bb1255d85c310
qzjul <jhaagsma@gmail.com>
2018-03-17 21:31:01 -0600

    fixing up colors and making it static

--- colors.php
+++ colors.php
@@ -1,53 +1,73 @@
-<?php namespace EENPC;
+<?php
+/**
+ * This class is to colour strings for the EE NPC's
+ *
+ * PHP Version 7
+ *
+ * @category Classes
+ * @package  EENPC
+ * @author   Julian Haagsma aka qzjul <jhaagsma@gmail.com>
+ * @license  All EENPC files are under the MIT License
+ * @link     https://github.com/jhaagsma/ee_npc
+ */
+
+namespace EENPC;
 
 class Colors
 {
-    private $foreground_colors = array();
-    private $background_colors = array();
+    // Set up shell colors
+    private $foreground_colors = [
+        'black' => '0;30',
+        'dark_gray' => '1;30',
+        'blue' => '0;34',
+        'light_blue' => '1;34',
+        'green' => '0;32',
+        'light_green' => '1;32',
+        'cyan' => '0;36',
+        'light_cyan' => '1;36',
+        'red' => '0;31',
+        'light_red' => '1;31',
+        'purple' => '0;35',
+        'light_purple' => '1;35',
+        'brown' => '0;33',
+        'yellow' => '1;33',
+        'light_gray' => '0;37',
+        'white' => '1;37',
+    ];
 
-    public function __construct()
-    {
-        // Set up shell colors
-        $this->foreground_colors['black']        = '0;30';
-        $this->foreground_colors['dark_gray']    = '1;30';
-        $this->foreground_colors['blue']         = '0;34';
-        $this->foreground_colors['light_blue']   = '1;34';
-        $this->foreground_colors['green']        = '0;32';
-        $this->foreground_colors['light_green']  = '1;32';
-        $this->foreground_colors['cyan']         = '0;36';
-        $this->foreground_colors['light_cyan']   = '1;36';
-        $this->foreground_colors['red']          = '0;31';
-        $this->foreground_colors['light_red']    = '1;31';
-        $this->foreground_colors['purple']       = '0;35';
-        $this->foreground_colors['light_purple'] = '1;35';
-        $this->foreground_colors['brown']        = '0;33';
-        $this->foreground_colors['yellow']       = '1;33';
-        $this->foreground_colors['light_gray']   = '0;37';
-        $this->foreground_colors['white']        = '1;37';
-
-        $this->background_colors['black']      = '40';
-        $this->background_colors['red']        = '41';
-        $this->background_colors['green']      = '42';
-        $this->background_colors['yellow']     = '43';
-        $this->background_colors['blue']       = '44';
-        $this->background_colors['magenta']    = '45';
-        $this->background_colors['cyan']       = '46';
-        $this->background_colors['light_gray'] = '47';
-    }//end __construct()
+    private $background_colors = [
+        'black' => '40',
+        'red' => '41',
+        'green' => '42',
+        'yellow' => '43',
+        'blue' => '44',
+        'magenta' => '45',
+        'cyan' => '46',
+        'light_gray' => '47',
+    ];
 
 
     // Returns colored string
-    public function getColoredString($string, $foreground_color = null, $background_color = null)
+    /**
+     * Colour a string
+     *
+     * @param  string $string           The string to clour
+     * @param  string $foreground_color The foreground colour
+     * @param  string $background_color The background colour
+     *
+     * @return string                   The coloured string
+     */
+    public static function getColoredString($string, $foreground_color = null, $background_color = null)
     {
         $colored_string = "";
 
         // Check if given foreground color found
-        if (isset($this->foreground_colors[$foreground_color])) {
-            $colored_string .= "\033[".$this->foreground_colors[$foreground_color]."m";
+        if (isset(self::$foreground_colors[$foreground_color])) {
+            $colored_string .= "\033[".self::$foreground_colors[$foreground_color]."m";
         }
         // Check if given background color found
-        if (isset($this->background_colors[$background_color])) {
-            $colored_string .= "\033[".$this->background_colors[$background_color]."m";
+        if (isset(self::$background_colors[$background_color])) {
+            $colored_string .= "\033[".self::$background_colors[$background_color]."m";
         }
 
         // Add string and end coloring
