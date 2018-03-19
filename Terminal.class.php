d0ea85ac6edb6fe5e9e68a7793d04f5126d01703
Julian Haagsma <jhaagsma@gmail.com>
2018-03-19 15:43:19 -0600

    add Terminal class; jam in some notes about eemphyre code standard

---
+++ Terminal.class.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * This class is for terminal output for the EE NPC's
+ *
+ * PHP Version 7
+ *
+ * @category Classes
+ * @package  EENPC
+ * @author   Julian Haagsma aka qzjul <jhaagsma@gmail.com>
+ * @license  All EENPC files are under the MIT License
+ * @link     https://github.com/jhaagsma/ee_npc
+ */
+
+namespace EENPC;
+
+class Terminal
+{
+
+
+}//end class
+
+    //add this and the other ones later...
+    //<rule ref="Squiz.WhiteSpace.ControlStructureSpacing"/>
+
+/*
+<?xml version="1.0" encoding="UTF-8"?>
+<ruleset name="emphyre">
+<description>The PHPCS standard, minus some breaking things.
+</description>
+    <!-- Include the whole PSR-1 standard -->
+    <rule ref="PSR1"/>
+    <!-- Include the whole PSR-2 standard -->
+    <rule ref="PSR2"/>
+
+<rule ref="Generic.ControlStructures.InlineControlStructure"/>
+<rule ref="Generic.Files.LineEndings"/>
+<rule ref="Generic.Formatting.DisallowMultipleStatements"/>
+<rule ref="Generic.Formatting.MultipleStatementAlignment">
+    <properties>
+        <property name="error" value="false"/>
+        <property name="maxPadding" value="40"/>
+    </properties>
+</rule>
+<rule ref="Generic.Formatting.NoSpaceAfterCast"/>
+<rule ref="Generic.Functions.CallTimePassByReference"/>
+<rule ref="Generic.Functions.FunctionCallArgumentSpacing"/>
+<rule ref="Generic.Metrics.CyclomaticComplexity"/>
+<rule ref="Generic.Metrics.NestingLevel"/>
+<rule ref="Generic.NamingConventions.ConstructorName"/>
+<rule ref="Generic.NamingConventions.UpperCaseConstantName"/>
+<rule ref="Generic.PHP.DeprecatedFunctions"/>
+<rule ref="Generic.PHP.DisallowShortOpenTag"/>
+<rule ref="Generic.PHP.ForbiddenFunctions">
+    <properties>
+        <property name="error" value="false"/>
+    </properties>
+</rule>
+<rule ref="Generic.PHP.LowerCaseConstant"/>
+<rule ref="Generic.PHP.NoSilencedErrors">
+    <properties>
+        <property name="error" value="false"/>
+    </properties>
+</rule>
+<rule ref="Generic.WhiteSpace.DisallowTabIndent"/>
+<rule ref="PEAR.Classes.ClassDeclaration"/>
+<rule ref="PEAR.Commenting.FileComment"/>
+<rule ref="PEAR.Commenting.FunctionComment"/>
+<rule ref="PEAR.Commenting.InlineComment"/>
+<rule ref="PEAR.Files.IncludingFile"/>
+<rule ref="PEAR.Formatting.MultiLineAssignment"/>
+<rule ref="PEAR.Functions.FunctionCallSignature"/>
+<rule ref="PEAR.Functions.ValidDefaultValue"/>
+<rule ref="PEAR.WhiteSpace.ScopeClosingBrace"/>
+<rule ref="Squiz.PHP.DisallowObEndFlush"/>
+<rule ref="Squiz.PHP.DisallowSizeFunctionsInLoops"/>
+<rule ref="Squiz.PHP.DiscouragedFunctions"/>
+<rule ref="Squiz.PHP.Eval"/>
+<rule ref="Squiz.PHP.ForbiddenFunctions"/>
+<rule ref="Squiz.PHP.GlobalKeyword"/>
+<rule ref="Squiz.PHP.InnerFunctions"/>
+<rule ref="Squiz.PHP.LowercasePHPFunctions"/>
+<rule ref="Squiz.Scope.StaticThisUsage"/>
+<rule ref="Squiz.WhiteSpace.CastSpacing"/>
+<rule ref="Squiz.WhiteSpace.ControlStructureSpacing"/>
+<rule ref="Squiz.WhiteSpace.LanguageConstructSpacing"/>
+<rule ref="Squiz.WhiteSpace.LogicalOperatorSpacing"/>
+<rule ref="Squiz.WhiteSpace.ObjectOperatorSpacing"/>
+<rule ref="Squiz.WhiteSpace.OperatorSpacing"/>
+<rule ref="Squiz.WhiteSpace.PropertyLabelSpacing"/>
+<rule ref="Zend.Debug.CodeAnalyzer"/>
+<rule ref="Squiz.Commenting.ClosingDeclarationComment" />
+</ruleset>
+ */
