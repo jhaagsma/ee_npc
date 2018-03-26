8bd925a76fe6a5df3f52bd5cd4e0545a2ebdb9d6
qzjul <jhaagsma@gmail.com>
2018-03-25 18:18:47 -0600

    government class

---
+++ Government.class.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file has all the government functions
+ *
+ * PHP Version 7
+ *
+ * @category Interface
+ * @package  EENPC
+ * @author   Julian Haagsma aka qzjul <jhaagsma@gmail.com>
+ * @license  MIT License
+ * @link     https://github.com/jhaagsma/ee_npc/
+ */
+
+namespace EENPC;
+
+class Government
+{
+    /**
+     * Change Government
+     *
+     * @param char $govt The government letter to change to
+     *
+     * @return $result Game Result
+     */
+    public static function change($govt)
+    {
+        $result = ee('govt', ['govt' => $govt]);
+        if (isset($result->govt)) {
+            out("Govt switched to {$result->govt}!");
+            $c = get_advisor();     //UPDATE EVERYTHING
+        }
+
+        return $result;
+    }//end change()
+}//end class
