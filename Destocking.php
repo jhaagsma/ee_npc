1e05c7f8b91598d54cb3d26f5ca3e678554b439b
jobbish-sql <33984311+jobbish-sql@users.noreply.github.com>
2021-03-25 15:19:16 -0500

    save an additional turn for techer destock

--- Destocking.php
+++ Destocking.php
@@ -42,8 +42,8 @@ function execute_destocking_actions($cnum, $strategy, $server, $rules, &$next_pl
 	// FUTURE: keep 8 turns for possible recall tech, recall goods, double sale of goods, double sale of tech
 	// however, we don't recall yet so set this to 2 turns
 	// want to be careful with the money to reserve unless we make ... if we have 200 bots all spending $50 million at the end then that's 10 B dollars on public
-	$turns_to_keep = 2;
-	$money_to_reserve = max(-2 * $c->income, 20000000); // mil expenses can rise rapidly during destocking, so use 10 M per turn as a guess
+	$turns_to_keep = 2 + ($strategy == 'T' ? 1 : 0); // save another turn for techer to dump tech
+	$money_to_reserve = max(-1 * $turns_to_keep * $c->income, 10000000 * $turns_to_keep); // mil expenses can rise rapidly during destocking, so use 10 M per turn as a guess
 	log_country_message($cnum, "Money is $c->money and calculated money to reserve is $money_to_reserve");
 	log_country_message($cnum, "Turns left: $c->turns");
 	log_country_message($cnum, "Starting cashing or teching...");	
